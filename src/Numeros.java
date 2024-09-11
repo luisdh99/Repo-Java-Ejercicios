100d64e23b39635bf1d8d4e422bc4bf7d5971773
luisdh <ldaniel.hernandez.h@gmail.com>
2024-09-11 15:48:37 -0600

    se suben los ejercicios de ciclos anidados

---
+++ src/Numeros.java
@@ -0,0 +1,13 @@
+
+public class Numeros {
+
+	public static void main(String[] args) {
+        for (int x = 1; x <= 3; x++) {
+            for (int y = 1; y <= 3; y++) {
+                System.out.println(x + " " + y);
+            }
+        }
+ 
+	}
+
+}
